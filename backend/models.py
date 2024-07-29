0df04a657a4e7a4f79b376a33daa76b826f51bc1
Mariana-Pizzi <143139979+Mariana-Pizzi@users.noreply.github.com>
2024-07-29 15:54:39 -0300

    update models

--- backend/models.py
+++ backend/models.py
@@ -2,25 +2,27 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Babosos(db.Model):
-    __tablename__ = 'baboso'
-    id_baboso = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(255), unique=True, nullable=False)
-
-class Datos_baboso(db.Model):
+class DatosBaboso(db.Model):
     __tablename__ = 'datos_baboso'
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(255), nullable=False)
-    encuentro = db.Column(db.String(255), nullable=False)
-    tiempo_declaracion = db.Column(db.String(255), nullable=False)
-    aparicion = db.Column(db.String(255), nullable=False)
-    frase_ingeniosa = db.Column(db.String(255), nullable=False)
+    lugar_acercamiento = db.Column(db.String(255), nullable=False)
+    tiempo_declararse = db.Column(db.String(255), nullable=False)
+    forma = db.Column(db.String(255), nullable=False)
+    frase = db.Column(db.String(255), nullable=False)
     respuesta = db.Column(db.String(255), nullable=False)
-    prop_indecentes = db.Column(db.String(255), nullable=False)
-    prop_previsibles = db.Column(db.String(255), nullable=False)
-    prop_divertidas = db.Column(db.String(255), nullable=False)
-    voto_autoestima = db.Column(db.Integer, nullable=False)
-    voto_insistente = db.Column(db.Integer, nullable=False)
-    voto_originalidad = db.Column(db.Integer, nullable=False)
-    conclusion = db.Column(db.String(255), nullable=False)
-    id_baboso = db.Column(db.Integer, db.ForeignKey('baboso.id_baboso'))
+    indecentes = db.Column(db.String(255), nullable=False)
+    previsibles = db.Column(db.String(255), nullable=False)
+    divertidos = db.Column(db.String(255), nullable=False)
+    autoestima = db.Column(db.Integer, nullable=False)
+    insistencia = db.Column(db.Integer, nullable=False)
+    originalidad = db.Column(db.Integer, nullable=False)
+    veredicto = db.Column(db.String(255), nullable=False)
+    baboso = db.relationship('Baboso', back_populates='datos_baboso', cascade='all, delete-orphan')
+
+class Baboso(db.Model):
+    __tablename__ = 'baboso'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(255))
+    datos_baboso_id = db.Column(db.Integer, db.ForeignKey('datos_baboso.id'), nullable=False)
+    datos_baboso = db.relationship('DatosBaboso', back_populates='baboso')
